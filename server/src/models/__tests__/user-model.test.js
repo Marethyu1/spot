8978c38a8888e33d7496b199745b754bd3c65b98
sha162 <sha162@uclive.ac.nz>
2018-08-30 16:50:14 +1200

    Added faker as dependency - Added updated test for user model - Added user prop generation method

--- server/src/models/__tests__/user-model.test.js
+++ server/src/models/__tests__/user-model.test.js
@@ -1,5 +1,7 @@
 const {setUp} = require("../../db/database-manager")
 const userModel = require("../user-model")
+const {generateUserProps} = require("../../../test/helper/prop-generation")
+const faker = require("faker")
 let db
 
 beforeAll(async () => {
@@ -11,15 +13,21 @@ afterAll(async () => {
 })
 
 
-test("I create a user", async () => {
-    const options = {
-        name: "james",
-        id: 12345,
-        email: "a.b@c.com"
-
-    }
-    const model  = await userModel.upsert(options)
+test("I can create a user", async () => {
+    const user = generateUserProps()
+    const model  = await userModel.upsert(user)
     expect(model).toBeTruthy()
-    expect(model.name).toBe(options.name)
+    expect(model.name).toBe(user.name)
+})
 
+test("I can update a user if one with the same id already exists", async () => {
+    const userProps = generateUserProps()
+    const createdUser = await userModel.upsert(userProps)
+    const updatedUser = {
+        ...userProps,
+        email: faker.internet.email(),
+    }
+    expect(updatedUser.email).not.toBe(createdUser.email)
+    const updatedModel = await userModel.upsert(updatedUser)
+    expect(updatedModel.email).toBe(updatedUser.email)
 })
---
+++ server/test/helper/prop-generation.js
@@ -0,0 +1,14 @@
+const faker = require("faker")
+
+
+const generateUserProps = () => {
+    return {
+        id: faker.random.number(),
+        name: faker.name.findName(),
+        email: faker.internet.email()
+    }
+}
+
+module.exports = {
+    generateUserProps
+}
