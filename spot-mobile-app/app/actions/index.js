525d0475465e7c26d925b8491778ac90dc486a6d
sha162 <sha162@uclive.ac.nz>
2018-10-26 15:09:10 +1300

    Refactored actions to fit style guide

--- spot-mobile-app/app/actions/index.js
+++ spot-mobile-app/app/actions/index.js
@@ -1,35 +1,34 @@
-//Imports all public actions from the actions directory
-//Exports all public actions for other files to import
+// Imports all public actions from the actions directory
+// Exports all public actions for other files to import
 
 import {
-    addDogs,
-    findDogs,
-    submitDog,
-    addSingleDog,
-    setCurrentDog,
-    updateDogTagAndClearCurrent
-} from "./dogsActions";
+  addDogs,
+  findDogs,
+  submitDog,
+  addSingleDog,
+  setCurrentDog,
+  updateDogTagAndClearCurrent,
+} from './dogsActions';
 
 import {
-    loginUser,
-} from "./userActions"
+  loginUser,
+} from './userActions';
 
 import {
-    setCameraPermission,
-    setLocationPermission
-} from "./permissionsActions"
+  setCameraPermission,
+  setLocationPermission,
+} from './permissionsActions';
 
 export {
-    addDogs,
-    addSingleDog,
-    findDogs,
-    submitDog,
-    setCurrentDog,
-    updateDogTagAndClearCurrent,
+  addDogs,
+  addSingleDog,
+  findDogs,
+  submitDog,
+  setCurrentDog,
+  updateDogTagAndClearCurrent,
 
-    loginUser,
+  loginUser,
 
-    setCameraPermission,
-    setLocationPermission,
-}
-
+  setCameraPermission,
+  setLocationPermission,
+};
