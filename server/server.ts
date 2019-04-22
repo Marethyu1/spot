fc1530024ed1dfcf2d55d99e13a1ec2ce210742f
sha162 <sha162@uclive.ac.nz>
2019-04-22 12:17:39 +1200

    added healthcheck to router

--- server/server.ts
+++ server/server.ts
@@ -11,6 +11,20 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
 })
 
+app.get('/health', (req: Request, res: Response) => {
+  res.send("Ok")
+})
+
+app.get('/health/db', (req: Request, res: Response) => {
+  const { getDatabase } = require('./src/db/database-manager')
+  try {
+    getDatabase()
+    res.send("OK")
+  } catch (err) {
+    res.status(500).send(err)
+  }
+})
+
 app.use(morgan('tiny'))
 app.use(`${BASE_URL}/users/`, userRouter)
 
