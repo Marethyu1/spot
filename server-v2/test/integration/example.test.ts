2f1b03c03c04cd8a176c534699770011f71a877e
Stefan Hall <stefan.hall@xero.com>
2019-04-27 17:21:30 +1200

    Added basic test examples

---
+++ server-v2/test/integration/example.test.ts
@@ -0,0 +1,5 @@
+describe('The example test', () => {
+    it('Should be able to add', () => {
+        expect(1 + 1).toBe(2)
+    })
+});
---
+++ server-v2/test/integration/health-check.test.ts
@@ -0,0 +1,7 @@
+import server from "../../src/server"
+
+describe('The health check route', function () {
+    it('Should return successfully', function () {
+        expect(1).toBe(1)
+    })
+});
