fc42003ead7567c00281b1fbafedf6c91c43cc1f
Stefan Hall <stefanhall@Stefans-MacBook-Pro.local>
2019-05-27 20:11:35 +1200

    ooh big pup

--- code-pipeline/routes/index.js
+++ code-pipeline/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hello big rig!!!' });
+  res.render('index', { title: 'One Day we gonna have some puppers in here!!!' });
 });
 
 module.exports = router;
